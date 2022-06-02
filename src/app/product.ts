b9c088595dda4ad6242f76bf7cb7d3a95156e4f0
ayknes <89252598+ayknes@users.noreply.github.com>
2022-06-02 10:49:45 +0200

    tt

--- src/app/product.ts
+++ src/app/product.ts
@@ -20,31 +20,31 @@ interface Graphique {
 export class product{
     marque: string;
     nom:string;
-        "type": "fixe",
-        "ecran": 0,
-        "clavier": 0,
-        "system": {
-            "ram": 32,
-            "cpu": {
-                "marque":"INTELLE",
-                "nom": "X9 900",
-                "score": 25000,
-                "core": 2,
-                "frequence-max": 5900,
-                "tdp": 125
+    type:string;
+    ecran:number;
+    clavier:string;
+    system: {
+            ram:number;
+            cpu: {
+                marque:string;
+                nom:string;
+                score:number;
+                core:number;
+                frequence_max:number;
+                tdp:number
             },
-            "hdd": {
-                "capacite": 3840,
-                "type": "nvme",
-                "rpm": 0,
-                "vitesse-transfert": 3000
+            hdd: {
+                capacite:number;
+                type:string;
+                rpm:number;
+                vitesse_transfert:number;
             },
-            "cg": {
-                "marque": "NVADIA",
-                "modele": "RTZ 3080",
-                "score": 20000
+            cg: {
+                marque: string;
+                modele: string;
+                score: number;
             }
-        },
-        "batterie" : 0,
-        "prix": 1000
+        };
+        batterie: number;
+        prix: number
 }
